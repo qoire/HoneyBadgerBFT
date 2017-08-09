bfd53994a606a195f852125d7fe06feea7a09313
qoire <mr.sun.yao@gmail.com>
2017-08-09 16:17:39 -0400

    message processor implemented

--- tools/runners/MsgProcessor.py
+++ tools/runners/MsgProcessor.py
@@ -6,62 +6,58 @@ from Queue import Queue
 from gevent import Greenlet
 
 
-'''
-To kill the necessary threads
-'''
+
 class PoisonPill:
+    '''
+    To kill the necessary threads
+    '''
     def __init__(self):
         pass
 
-'''
-A block, created from a list of transactions (hashes), assumes that
-the transactions are encoded in binary format
-'''
-class Block:
-    def __init__(self, txs):
-        self.txs = txs
 
-'''
-MsgProcessor class, processes messages between the other components of the kernel
-and the consensus algorithms (this), defines a few message types for passing
-messages between the two parts of the system, this subroutine should be booted-up
-alongside the kernel, and assigned a specific socket port
-
-Composites two greenlets that does most of the heavy lifting. Establishes a two
-way connection.
-
-Connection phase happens as such:
-1) The other party sets up server first, then calls us (launches)
-2) We set up our server, then connect to their server, and send the message:
-    {
-        msg_type: CONNECT
-        host: 127.0.0.1
-        port: 30303 (or something like that)
-    }
-
-SERVER_SIDE:
-Messages should be in the following format:
-    {
-        msg_type: TX (so far only transaction messages supported),
-        msg_contents: TX_BODY
-    }
-
-(we dont have any failure messages right now)
-
-
-CLIENT_SIDE:
-At an undetermined future point in time, the we should respond with:
-    {
-        msg_type: BLK,
-        msg_contents: BLK_BODY
-    }
-
-where BLK_BODY, is a very simplified block (no state validation or anything):
-    {
-        txs: [TX_HASH_LIST]
-    }
-'''
 class MsgProcessor():
+    '''
+    MsgProcessor class, processes messages between the other components of the kernel
+    and the consensus algorithms (this), defines a few message types for passing
+    messages between the two parts of the system, this subroutine should be booted-up
+    alongside the kernel, and assigned a specific socket port
+
+    Composites two greenlets that does most of the heavy lifting. Establishes a two
+    way connection.
+
+    Connection phase happens as such:
+    1) The other party sets up server first, then calls us (launches)
+    2) We set up our server, then connect to their server, and send the message:
+        {
+            msg_type: CONNECT
+            host: 127.0.0.1
+            port: 30303 (or something like that)
+        }
+
+    3) At which point we assume the other side has connected to us, proceed as usual
+
+    SERVER_SIDE:
+    Messages should be in the following format:
+        {
+            msg_type: TX (so far only transaction messages supported),
+            msg_contents: TX_BODY
+        }
+
+    (we dont have any failure messages right now)
+
+
+    CLIENT_SIDE:
+    At an undetermined future point in time, the we should respond with:
+        {
+            msg_type: BLK,
+            msg_contents: BLK_BODY
+        }
+
+    where BLK_BODY, is a very simplified block (no state validation or anything):
+        {
+            txs: [TX_HASH_LIST]
+        }
+    '''
     MSG_SIZE = 4096
 
     def __init__(self, conn = ('127.0.0.1', 30302), serv_conn = ('127.0.0.1', 30303)):
@@ -71,28 +67,36 @@ class MsgProcessor():
         self.serv_conn = serv_conn
         self.q = Queue()
 
-    '''
-    Runs the message processor, no lightweight threads are started until this point
-    '''
+        self.server_shutdown = False
+        self.client_shutdown = False
+
+        self.server_t = None
+        self.client_t = None
+
+
     def run(self):
+        '''
+        Runs the message processor, no lightweight threads are started until this point
+        '''
         Greenlet(self._run()).start()
 
-    '''
-    TODO: add exception handling
-    '''
+
     def _run(self):
-        context = zmq.Context()
+        '''
+        TODO: add exception handling
+        '''
+        self.context = zmq.Context()
 
         def _server():
             print("MsgProcessor: server running")
-            socket = context.socket(zmq.PAIR)
+            socket = self.context.socket(zmq.PAIR)
             socket.bind("tcp://127.0.0.1:%s" % self.serv_conn[1])
             print("MsgProcessor: server -> bound")
             return socket
 
         def _client():
             print("MsgProcessor: client running")
-            socket = context.socket(zmq.PAIR)
+            socket = self.context.socket(zmq.PAIR)
             socket.connect("tcp://localhost:%s" % self.conn[1])
             print("MsgProcessor: client -> connection established")
             return socket
@@ -106,48 +110,88 @@ class MsgProcessor():
         ret = gevent.joinall([g_s, g_c])
         client_sock = ret[1].value
 
+        # lets also bind to class
+        self.server_sock = ret[0].value # type: zmq.socket
+        self.client_sock = ret[1].value # type: zmq.socket
+
         # now all connections are established, run the routines
         print("MsgProcessor: connection, established, sending counterparty information")
+        client_sock.send(json.dumps({'msg_type': 'CONNECT', 'ip': 'localhost', 'port': 30303}))
 
-        def _notify(sock):
-            sock.send(json.dumps({'msg_type': 'CONNECT', 'ip': 'localhost', 'port': 30303}))
-        Greenlet(_notify, client_sock).start()
+        self.server_t = Greenlet(self._client)
+        self.client_t = Greenlet(self._server)
+
+        self.server_t.start()
+        self.client_t.start()
 
 
-    '''
-    Client routine, defines an inbound queue (from our perspective),
-    the only message that we support currently is BLK
-    '''
-    def _client(self, socket, iq):
-        client = socket
-        print("MsgProcessor: client (us -> kernel) routine active")
-        while True:
-            # expects a json string here, nothing else (exception is poison pill)
-            item = iq.get()
-            if isinstance(item, PoisonPill):
+    def _client(self):
+        '''
+        Client routine, defines an inbound queue (from our perspective),
+        the only message that we support currently is BLK
+        '''
+
+        client = self.client_sock
+
+        if client == None:
+            raise RuntimeError("client_sock not connected")
+
+        print("MsgProcessor: client (us -> kernel) subroutine active")
+
+        while not self.client_shutdown:
+            msg = self.q.get()
+
+            if isinstance(msg, PoisonPill):
                 break
-            client.sendall(item)
-        client.shutdown()
-        client.close()
+
+            # expecting a dictionary here thats serializable
+            client.send(json.dumps(msg))
+
+        print("MsgProcessor: client shutdown")
 
 
-    '''
-    Server routine, defines an outbound queue, directly triggers
-    '''
-    def _server(self, socket):
-        print("MsgProcessor: server (kernel -> us) routine active")
+    def _server(self):
+        '''
+        Server routine, defines an outbound queue, directly triggers
+        '''
+        server = self.server_sock
 
-        while True:
-            pass
+        if server == None:
+            raise RuntimeError("server_sock not connected")
 
-        socket.shutdown()
-        socket.close()
+        print ("MsgProcessor: server (kernel -> us) subroutine active")
 
-    # expects a list of tx_list in hexstring format
+        while not self.server_shutdown:
+            msg = server.get()
+            obj = json.loads(msg)
+
+            if (obj['msg_type'] == 'DISCONNECT'):
+                # we're being disconnected, send poison pill to client and disconnect
+                self.q.put(PoisonPill())
+                break
+            elif (obj['msg_type'] == 'TX'):
+                tx_hash = obj['tx_hash'] # expected 32-byte hash
+                # TODO: hook up to rest of system
+
+        print("MsgProcessor: server shutdown")
+
+
     def send_block(self, tx_list, binary=False):
         if binary:
             tx_list = [a.encode('hex_codec') for a in tx_list]
         self.q.put(json.dumps({'msg_type': 'BLK', 'tx_list': tx_list}))
 
     def stop(self):
-        self.shutdown = True
+        self.server_shutdown = True
+        self.client_shutdown = True
+
+        gevent.joinall([self.server_t, self.client_t])
+
+        if self.server_sock != None:
+            self.server_sock.close()
+
+        if self.client_sock != None:
+            self.client_sock.close()
+
+        if self.context != None:
+            self.context.term()
--- tools/runners/MsgProcessor_test.py
+++ tools/runners/MsgProcessor_test.py
@@ -1,6 +1,7 @@
 import unittest
 from MsgProcessor import MsgProcessor
 import zmq
+import json
 
 class TestMsgProcessor(unittest.TestCase):
 
@@ -16,4 +17,31 @@ class TestMsgProcessor(unittest.TestCase):
 
         while True:
             msg = socket.recv()
-            print(msg)
+            if msg != None:
+                break
+
+        obj = json.loads(msg)
+        print(obj)
+        if not 'msg_type' in obj:
+            self.fail("msg_type not a key")
+
+        if not 'ip' in obj:
+            self.fail("ip not a key")
+
+        if not 'port' in obj:
+            self.fail("port not a key")
+
+        proc.stop()
+
+        # shutdown our sockets too
+        socket.close()
+        context.term()
+
+    def test_message_sending(self):
+        port = 30302 # default
+        context = zmq.Context()
+        socket = context.socket(zmq.PAIR)
+        socket.bind("tcp://127.0.0.1:%s" % port)
+
+        proc = MsgProcessor()
+        proc.run()
